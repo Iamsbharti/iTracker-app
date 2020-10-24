eb806ec0284d56f019908ffd8e7b79d64a24e95c
Iamsbharti <saurabhbharti9@gmail.com>
2020-10-25 00:53:38 +0530

    app-structure-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './user/login/login.component';
 import { SignupComponent } from './user/signup/signup.component';
+import { HomeComponent } from './home/home.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
   { path: 'signup', component: SignupComponent, pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, pathMatch: 'full' },
   { path: '404', component: NotfoundComponent },
   { path: '**', redirectTo: '/404' },
 ];
